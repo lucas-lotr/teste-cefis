5d9a4fa69c9dcfdcde86a5c48a2578909a3b1a80
lucas-lotr <44841472+lucas-lotr@users.noreply.github.com>
2018-12-27 15:33:41 -0200

    db loading

--- src/components/ListView/ListView.js
+++ src/components/ListView/ListView.js
@@ -1,22 +1,77 @@
 import React, { Component } from "react";
+import LoadIcon from "../LoadIcon/LoadIcon";
 
 class ListView extends Component {
+  state = {
+    loaded: false,
+    db: {}
+  };
+
+  componentDidMount() {
+    if (!this.state.loaded) {
+      //   console.log("load");
+      document.addEventListener("updateDB", this.updateDB);
+      this.loadDB();
+    }
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("updateDB", this.updateDB);
+  }
+
+  updateDB = data => {
+    // console.log(data.detail);
+    document.removeEventListener("updateDB", this.updateDB);
+    this.state.db = data.detail;
+    this.state.loaded = true;
+    this.setState(this.state);
+  };
+
+  loadDB = () => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        // console.log(JSON.parse(this.responseText).data);
+        // updateDB(JSON.parse(this.responseText).data);
+        // this.state.db = JSON.parse(this.responseText).data
+        // this.stat
+        // this.setState()
+        let response = JSON.parse(this.responseText).data;
+
+        // console.log("done");
+
+        document.dispatchEvent(
+          new CustomEvent("updateDB", {
+            detail: response
+          })
+        );
+      }
+    };
+    xhttp.open("GET", "https://cefis.com.br/api/v1/event", true);
+    xhttp.send();
+  };
+
   handleContentClick = event => {
     window.location.search = `?id=${event.target.id}`;
   };
 
   render() {
-    return (
-      <div>
-        <p>ListView</p>
-        <button id="1" onClick={this.handleContentClick}>
-          id 1
-        </button>
-        <button id="2" onClick={this.handleContentClick}>
-          id 2
-        </button>
-      </div>
-    );
+    console.log(this.state.db);
+    if (this.state.loaded) {
+      return (
+        <div id="list-view">
+          <p>ListView</p>
+          <button id="1" onClick={this.handleContentClick}>
+            id 1
+          </button>
+          <button id="2" onClick={this.handleContentClick}>
+            id 2
+          </button>
+        </div>
+      );
+    } else {
+      return <LoadIcon />;
+    }
   }
 }
 
---
+++ src/components/LoadIcon/LoadIcon.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class LoadIcon extends Component {
+  render() {
+    return (
+      <div>
+        <p>LOADING...</p>
+      </div>
+    );
+  }
+}
+
+export default LoadIcon;
